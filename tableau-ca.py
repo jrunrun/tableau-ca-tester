20994987435802b9d83375ab1e1db254817546e4
Justin Craycraft <jpcraycraft@gmail.com>
2022-07-15 01:45:19 +0000

    -added non sensitive info back to config

--- tableau-ca.py
+++ tableau-ca.py
@@ -81,8 +81,8 @@ def auth_CA_JWT_RestAPI(ts_server, ts_site, ts_username, ts_ca_iss, ts_ca_kid, t
 
 ts_server =''
 ts_site = ''
-ts_username = ''
-ts_serverType = ''
+ts_username = 'n639986'
+ts_serverType = 'TOL'
 ts_ca_iss =''
 ts_ca_kid =''
 ts_ca_secret = ''
