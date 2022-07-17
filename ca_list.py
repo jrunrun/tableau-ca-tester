055efaf4d4cd825c8017938354eb90020e44c2b9
Justin Craycraft <jpcraycraft@gmail.com>
2022-07-17 13:13:26 -0500

    -place holder for CAs

---
+++ ca_list.py
@@ -0,0 +1,45 @@
+ca_list = [
+
+    {
+        'ts_server': '',
+        'ts_site': '',
+        'ts_username': '',
+        'ts_serverType': 'TOL',
+        'ts_ca_iss': '',
+        'ts_ca_kid': '',
+        'ts_ca_secret': '',
+        'ts_api_version': '3.16'
+    },
+    {
+
+        'ts_server': '',
+        'ts_site': '',
+        'ts_username': '',
+        'ts_serverType': 'TOL',
+        'ts_ca_iss': '',
+        'ts_ca_kid': '',
+        'ts_ca_secret': '',
+        'ts_api_version': '3.16'
+    },
+    {
+        'ts_server': '',
+        'ts_site': '',
+        'ts_username': '',
+        'ts_serverType': 'TOL',
+        'ts_ca_iss': '',
+        'ts_ca_kid': '',
+        'ts_ca_secret': '',
+        'ts_api_version': '3.16'
+    },
+    {
+        'ts_server': '',
+        'ts_site': '',
+        'ts_username': '',
+        'ts_serverType': 'TOL',
+        'ts_ca_iss': '',
+        'ts_ca_kid': '',
+        'ts_ca_secret': '',
+        'ts_api_version': '3.16'
+    }
+
+]
